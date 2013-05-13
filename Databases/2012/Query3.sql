d60a1f3a65a74986cc83e25f8cdd4578ec4e8189
Colin Fitzsimons <colin.altered@gmail.com>
2013-05-13 16:48:51 +0100

    FailFish

--- Databases/2012/Query3.sql
+++ Databases/2012/Query3.sql
@@ -1,2 +1,6 @@
-SELECT * FROM arrivale, airport
-WHERE
+SELECT * FROM arrival, airport
+WHERE arrival.code = airport.code
+AND arrival.day = '2002-1-13'
+AND arrival.hour >= '10:00'
+AND arrival.hour <= '12:00'
+ORDER BY arrival.no, airport.name
